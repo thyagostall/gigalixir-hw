9592a8fe177199fa3dd8e2c4b23062342725d628
Thyago Stall <thstall@gmail.com>
2018-03-30 18:14:12 -0300

    Video category on form

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -2,6 +2,9 @@ defmodule GigalixirHelloworld.VideoController do
   use GigalixirHelloworld.Web, :controller
 
   alias GigalixirHelloworld.Video
+  alias GigalixirHelloworld.Category
+
+  plug :load_categories when action in [:new, :create, :edit, :update]
 
   def index(conn, _params, user) do
     videos = Repo.all(user_videos(user))
@@ -72,5 +75,14 @@ defmodule GigalixirHelloworld.VideoController do
 
   defp user_videos(user) do
     assoc(user, :videos)
+  end
+
+  defp load_categories(conn, _) do
+    query = Category
+    |> Category.names_and_ids
+    |> Category.alphabetical
+
+    categories = Repo.all(query)
+    assign(conn, :categories, categories)
   end
 end
