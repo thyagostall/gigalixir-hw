cb98b9d269b82e2a9c1a2b931bd05d2dd4ca2120
Thyago Stall <thstall@gmail.com>
2018-04-08 19:56:22 -0300

    Fixes page controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule GigalixirHelloworld.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Gigalixir Hello World!"
   end
 end
